9ae362131291aa720676668fc371e689a8575bdd
Jake Sower <jakesower@gmail.com>
2017-01-16 17:09:03 -0500

    tweaking

--- widgets/test-chat.js
+++ widgets/test-chat.js
@@ -1,17 +1,27 @@
 window.adhoc.widget = function( connection, rootElement ) {
-  rootElement.innerHTML = '<div id="debug"></div>';
+  rootElement.innerHTML = `
+    <input type="text" id="input"> <button id="send">Send</button>
+    <div id="debug"></div>
+  `
 
-  appendDebug = function( m ) {
+  var debugDiv = document.querySelector( '#debug' );
+  var input = document.querySelector( '#input ');
+  var send = document.querySelector( '#send' );
+
+  send.addEventListener( 'click', function( e ) {
+    connection.send( input.value );
+    input.value = '';
+  })
+
+  appendDebug = function( type, m ) {
+    console.log( m )
     var c = document.createElement('div');
-    c.innerHTML = m;
+    c.innerHTML = type + ': ' + m;
     rootElement.appendChild(c)
   }
 
-  connection.onmessage = appendDebug;
-  connection.onsignal = appendDebug;
-
-  connection.send('moo');
-  connection.signal('bock')
+  connection.onmessage = m => appendDebug( 'message', m.data );
+  connection.onsignal = m => appendDebug( 'signal', m );
 
   window.woot = connection
   return connection;
